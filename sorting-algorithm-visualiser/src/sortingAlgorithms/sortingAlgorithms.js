6d77be5f1af86fbaf47ef965fd506ee1594ca687
Alvin-Fung <alvin_fung9@outlook.com>
2023-12-08 16:45:21 +0000

    mergeSort refactored and mergeSortHelper created

--- sorting-algorithm-visualiser/src/sortingAlgorithms/sortingAlgorithms.js
+++ sorting-algorithm-visualiser/src/sortingAlgorithms/sortingAlgorithms.js
@@ -1,12 +1,20 @@
-export const getMergeSortAnimations
-
-export const mergeSort (array) => {
+export function mergeSort(array) {
+    const animations = [];
     if (array.length <= 1) return array;
     const auxiliaryArray = array.slice();
-    mergeSortHelper()
+    mergeSortHelper(array, 0, array.length - 1, auxiliaryArray, animations);
+    return animations;
 };
 
 //Apparently algoexpert.io/clem website won't allow me to understand this tutorials version of a merge sort as it's a paid product :(
-function mergeSortHelper() {
-
+function mergeSortHelper(
+    mainArray,
+    startIdx,
+    endIdx,
+    auxiliaryArray,
+    animations) {
+    if (startIdx === endIdx) return;
+    const middleIdx = Math.floor((startIdx + endIdx) / 2);
+    mergeSortHelper(auxiliaryArray, startIdx, middleIdx, mainArray, animations);
+    mergeSortHelper(auxiliaryArray, middleIdx, endIdx, auxiliaryArray, animations);
 }
